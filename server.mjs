b3d83bf7f910d7ff6fc915cb315d76bd58b74612
atif memon <memonatif690@gmail.com>
2022-08-23 20:40:30 +0500

    update

--- server.mjs
+++ server.mjs
@@ -1,9 +1,8 @@
-console.log("Bello!");
-console.log(nanoid());
-
 import express from "express";
 import cors from "cors";
 import { nanoid } from "nanoid";
+import mongoose from "mongoose";
+import { stringToHash, varifyHash } from "bcrypt-inzi";
 
 const app = express();
 app.use(express.json());
@@ -11,14 +10,23 @@ app.use(cors());
 
 const port = process.env.PORT || 3000;
 
-let userBase = [];
+const userSchema = new mongoose.Schema({
+  firstName: { type: String },
+  lastName: { type: String },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
 
-//Signup Database
+  age: { type: Number, min: 17, max: 65, default: 18 },
+  isMarried: { type: Boolean, default: false },
+
+  createdOn: { type: Date, default: Date.now },
+});
+const userModel = mongoose.model("User", userSchema);
+
 app.post("/signup", (req, res) => {
   let body = req.body;
 
   if (!body.firstName || !body.lastName || !body.email || !body.password) {
-    //Status(400) is use for bad request (Client Error).
     res.status(400).send(
       `required fields missing, request example: 
                 {
@@ -31,64 +39,91 @@ app.post("/signup", (req, res) => {
     return;
   }
 
-  let isFound = false;
+  // check if user already exist // query email user
+  userModel.findOne({ email: body.email }, (err, data) => {
+    if (!err) {
+      console.log("data: ", data);
 
-  for (let i = 0; i < userBase.length; i++) {
-    if (userBase[i].email === body.email.toLowerCase()) {
-      isFound = true;
-      break;
+      if (data) {
+        // user already exist
+        console.log("user already exist: ", data);
+        res
+          .status(400)
+          .send({
+            message: "user already exist,, please try a different email",
+          });
+        return;
+      } else {
+        // user not already exist
+
+        stringToHash(body.password).then((hashString) => {
+          let newUser = new userModel({
+            firstName: body.firstName,
+            lastName: body.lastName,
+            email: body.email.toLowerCase(),
+            password: hashString,
+          });
+          newUser.save((err, result) => {
+            if (!err) {
+              console.log("data saved: ", result);
+              res.status(201).send({ message: "user is created" });
+            } else {
+              console.log("db error: ", err);
+              res.status(500).send({ message: "internal server error" });
+            }
+          });
+        });
+      }
+    } else {
+      console.log("db error: ", err);
+      res.status(500).send({ message: "db error in query" });
     }
-  }
-  if (isFound) {
-    res.status(400).send({
-      message: `email ${body.email} already exist.`,
-    });
-    return;
-  }
-
-  let newUser = {
-    userId: nanoid(),
-    firstName: body.firstName,
-    lastName: body.lastName,
-    email: body.email.toLowerCase(),
-    password: body.password,
-  };
-
-  userBase.push(newUser);
-  // status(201) is used to send succeed message;
-  res.status(201).send({ message: "user is created" });
+  });
 });
 
-//Login Database
+app.get("/users", async (req, res) => {
+  try {
+    let allUser = await userModel.find({}).exec();
+    res.send(allUser);
+  } catch (error) {
+    res.status(500).send({ message: "error getting users" });
+  }
+});
 
 app.post("/login", (req, res) => {
   let body = req.body;
 
   if (!body.email || !body.password) {
+    // null check - undefined, "", 0 , false, null , NaN
     res.status(400).send(
-      `Required fields missing, Request example:
-    {
-        "email": "abc@abc.com",
-        "password": "12345"
-    }`
+      `required fields missing, request example: 
+                {
+                    "email": "abc@abc.com",
+                    "password": "12345"
+                }`
     );
     return;
   }
 
-  let isFound = false;
+  let isFound = false; // https://stackoverflow.com/a/17402180/4378475
 
   for (let i = 0; i < userBase.length; i++) {
     if (userBase[i].email === body.email) {
       isFound = true;
       if (userBase[i].password === body.password) {
+        // correct password
+
         res.status(200).send({
           firstName: userBase[i].firstName,
           lastName: userBase[i].lastName,
           email: userBase[i].email,
           message: "login successful",
+          token: "some unique token",
         });
         return;
       } else {
+        // password incorrect
+
         res.status(401).send({
           message: "incorrect password",
         });
@@ -108,3 +143,36 @@ app.post("/login", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+let dbURI =
+  "mongodb+srv://atif:memon1@cluster0.vjyzoga.mongodb.net/serverdatabase?retryWrites=true&w=majority";
+mongoose.connect(dbURI);
+
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+mongoose.connection.on("connected", function () {
+  //connected
+  console.log("Mongoose is connected");
+});
+
+mongoose.connection.on("disconnected", function () {
+  //disconnected
+  console.log("Mongoose is disconnected");
+  process.exit(1);
+});
+
+mongoose.connection.on("error", function (err) {
+  //any error
+  console.log("Mongoose connection error: ", err);
+  process.exit(1);
+});
+
+process.on("SIGINT", function () {
+  /////this function will run jst before app is closing
+  console.log("app is terminating");
+  mongoose.connection.close(function () {
+    console.log("Mongoose default connection closed");
+    process.exit(0);
+  });
+});
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
